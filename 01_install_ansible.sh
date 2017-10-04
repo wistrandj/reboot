723a25c68cd9e931249d7b0d2260461ab62ca6be
Jasse Wistrand <jasse.wistrand@gmail.com>
2017-10-04 18:57:45 +0100

    fix vagrant installation

--- 01_install_ansible.sh
+++ 01_install_ansible.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 
-echo "deb http://ppa.launchpad.net/ansible/ansible/ubuntu trusty main" > "/etc/apt/sources.list.d/ansible.list"
+keyurl="http://ppa.launchpad.net/ansible/ansible/ubuntu"
+path="/etc/apt/sources.list.d/ansible.list"
+
+echo "deb $keyurl trusty main" > $path
 apt-key adv --keyserver keyserver.ubuntu.com --recv-keys 93C4A3FD7BB9C367
-apt-get update
-apt-get install ansible
+apt-get update -y
+apt-get install -y ansible
